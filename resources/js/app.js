aa17a24e276f051c4759ab2f338136a3a2c07aa0
Konstantin Knopp <k.knopp@das-unikat.com>
2021-02-05 10:56:43 +0100

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 require('./bootstrap');
-require('./jquery.min');
+// require('./jquery.min');
 require('./bootstrap.min');
 require('./plugins');
 require('./main');
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,7 @@
 window._ = require('lodash');
 
 try {
-    require("jquery");
+    window.jQuery = require("jquery");
 } catch (e) {
 }
 
